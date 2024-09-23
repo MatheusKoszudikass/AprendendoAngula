0390ed0c515e5582abfda173967fd70ba45e3ec3
Matheus Gomes <matheusprgc@gmail.com>
2024-09-23 16:47:39 -0300

    chore: Código refatorado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,22 +4,16 @@ import { HomeComponent } from './component/home/home.component';
 import { UserComponent } from './component/user/user.component';
 import { FooterComponent } from "./component/footer/footer.component";
 import { ImgComponent } from "./component/img/img.component";
+import { ChildComponent } from './component/img/child/child.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent, HomeComponent, FooterComponent, ImgComponent],
-  // templateUrl: './app.component.html',
-  // styleUrl: './app.component.scss'
-  template: `Hello {{city}}, {{50 + 50}}, 
-  <section><app-user/></section> 
-  <section><app-home/></section>
-  <section><app-footer/></section>
-  <section><app-img/></section>`,
-  styles: `:host {
-     color: #a144eb;
-  }`
+  imports: [RouterOutlet, UserComponent, HomeComponent, 
+    FooterComponent, ImgComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ativo-byte';
