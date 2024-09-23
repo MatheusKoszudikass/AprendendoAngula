dcd2900cdf0edf4bb84c97f22eb81c9d87f472c0
Matheus Gomes <matheusprgc@gmail.com>
2024-09-23 16:59:00 -0300

    feat: Utilizando a classe NgOptimizedImage para melhor desempenho de imagens. Alguns metodos para fixação dos aprendizados.

--- src/app/component/img/img.component.ts
+++ src/app/component/img/img.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { ChildComponent } from "./child/child.component";
+
 
 @Component({
   selector: 'app-img',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, ChildComponent],
   templateUrl: './img.component.html',
   styleUrl: './img.component.scss'
 })
 export class ImgComponent {
-  name = 'Matheus edit';
-  imageURL = true;
+
+  items = new Array<number>();
+  stringArray = new Array<string>();
+
+  message: string = '';
+  logoUrl: string = '/img/LogoTipo.png';
+  logoAlt: string = 'AtivoByte';
+  isEditable: boolean = true;
+
+  @Input() theme: string = 'dark';
+
+  onMouseOver() {
+    this.message = 'Way to  go 🐂';
+    console.log(this.message);
+  }
+
+  greet(teste: string) {
+    this.isEditable = false;
+    console.log(this.isEditable);
+    console.log(teste);
+  }
+
+  addItem(item: number) {
+      this.items.push(item);
+      this.items.forEach(item => {
+        console.log('Tipo', typeof item);
+      })
+
+      this.stringArray = this.items.map(String); 
+      this.stringArray.forEach(item => {
+        console.log('Tipo', typeof item);
+      })
+
+      console.log('item adicionado', item);
+  }
 }
